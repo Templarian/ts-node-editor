a32dd2f256a7dffe99134aec7d4e3ba025453c6f
Austin Andrews <admin@templarian.com>
2024-01-29 23:50:42 -0600

    Update helloWorld.spec.ts

--- src/scripts/helloWorld.spec.ts
+++ src/scripts/helloWorld.spec.ts
@@ -1,8 +1,8 @@
 import { describe, expect, test } from '@jest/globals';
 import { run } from './helloWorld';
 
-describe('sum module', () => {
-  test('verify basic run', async () => {
+describe('helloWorld', () => {
+  test('should run and get message', async () => {
     const state = new Map();
     state.set('name', 'World');
     const result = await run(state);
