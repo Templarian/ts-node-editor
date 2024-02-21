94c2340464b11072b72dd5a27000421fc318179f
Austin Andrews <admin@templarian.com>
2024-02-20 22:56:37 -0600

    More fixes

--- packages/ts-node-editor/src/client/utils/contextMenu.ts
+++ packages/ts-node-editor/src/client/utils/contextMenu.ts
@@ -1,23 +1,14 @@
 import UiMenu from '../ui/menu/menu';
-import UiMenuItem from '../ui/menuItem/menuItem';
-import UiMenuSeperator from '../ui/menuSeperator/menuSeperator';
 
-function handleContextMenu(e: any) {
+function handleContextMenu(e: any, options: () => any[]) {
     if (e.currentTarget !== e.target) {
         return;
     }
     e.preventDefault();
     const $menu = document.createElement('ui-menu') as UiMenu;
-    $menu.options = [{
-        type: UiMenuItem,
-        label: 'testing',
-        key: 0
-    }, {
-        type: UiMenuSeperator,
-        key: 1
-    }];
     $menu.style.setProperty('--ui-menu-x', `${e.pageX}`);
     $menu.style.setProperty('--ui-menu-y', `${e.pageY}`);
+    $menu.options = options();
     $menu.addEventListener('select', (e: any) => {
         const { item } = e.detail;
     });
@@ -29,8 +20,10 @@ function handleContextMenu(e: any) {
  * @param element Attach to a part in connectedCallback
  * @param options Ex: [{ label, value }, null]
  */
-export function wireContextMenu(element: HTMLElement, options: () => {}) {
-    element.addEventListener('contextmenu', handleContextMenu);
+export function wireContextMenu(element: HTMLElement, options: () => any[]) {
+    element.addEventListener('contextmenu', function (e) {
+        handleContextMenu(e, options);
+    });
 }
 
 export function unwireContextMenu(element: HTMLElement) {
