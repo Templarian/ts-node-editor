e57fba9cd53debd1f98f4cbf6e0c0f9bd1a9a3e9
Austin Andrews <admin@templarian.com>
2024-04-05 22:33:31 -0500

    Cleanup

--- packages/ts-node-editor/src/client/app/index/index.ts
+++ packages/ts-node-editor/src/client/app/index/index.ts
@@ -4,6 +4,8 @@ import template from "./index.html";
 import style from './index.css';
 
 import UiGrid from '../../ui/grid/grid';
+import UiButton from '../../ui/button/button';
+import UiIcon from '../../ui/icon/icon';
 
 @Component({
   selector: 'app-index',
@@ -12,6 +14,13 @@ import UiGrid from '../../ui/grid/grid';
 })
 export default class AppIndex extends HTMLElement {
   @Part() $grid: UiGrid;
+
+  @Part() $debug: UiButton;
+  @Part() $debugCheck: UiIcon;
+  @Part() $debugPlay: UiButton;
+  @Part() $debugPrevious: UiButton;
+  @Part() $debugNext: UiButton;
+  @Part() $debugRestart: UiButton;
 
   connectedCallback() {
     document.addEventListener('wheel', (e) => {
@@ -48,6 +57,39 @@ export default class AppIndex extends HTMLElement {
       $width: 10,
       $height: 6
     });*/
+
+    // Debug
+    this.$debug.addEventListener('click', this.handleDebug.bind(this));
+    this.$debugPlay.addEventListener('click', this.handlePlay.bind(this));
+    this.$debugPrevious.addEventListener('click', this.handlePrevious.bind(this));
+    this.$debugNext.addEventListener('click', this.handleNext.bind(this));
+    this.$debugRestart.addEventListener('click', this.handleRestart.bind(this));
+  }
+
+  #debug = false;
+  handleDebug() {
+    this.#debug = !this.#debug;
+    if (this.#debug) {
+      this.$debugCheck.name = 'checkbox-marked';
+    } else {
+      this.$debugCheck.name = 'checkbox-blank';
+    }
+  }
+
+  handlePlay() {
+    console.log('play');
+  }
+
+  handlePrevious() {
+    console.log('previous');
+  }
+
+  handleNext() {
+    console.log('next');
+  }
+
+  handleRestart() {
+    console.log('restart');
   }
 
   render() {
