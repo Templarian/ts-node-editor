897eadf8217b7a9dce2002e66f499ff6b93fd914
Austin Andrews <admin@templarian.com>
2024-04-05 21:12:50 -0500

    a star path finding

--- packages/ts-node-editor/src/client/utils/mouse.spec.ts
+++ packages/ts-node-editor/src/client/utils/mouse.spec.ts
@@ -22,7 +22,7 @@ describe('mouse', () => {
     expect(JSON.stringify(result)).toBe(JSON.stringify(expects));
   });
 
-  it('should navigate north first', () => {
+  it('should navigate south first', () => {
     const matrix = [
       [0, 0, 0, 0],
       [0, 1, 1, 0],
@@ -31,13 +31,13 @@ describe('mouse', () => {
     ];
     const result = getPath(matrix, { x: 0, y: 1 }, { x: 3, y: 2 });
     const expects = [
-      { x: 1, y: 0 },
-      { x: 2, y: 0 },
-      { x: 3, y: 0 },
-      { x: 3, y: 1 },
-      { x: 3, y: 2 },
+      { x: 0, y: 1 },
+      { x: 0, y: 2 },
+      { x: 0, y: 3 },
+      { x: 1, y: 3 },
+      { x: 2, y: 3 },
       { x: 3, y: 3 },
-      { x: 2, y: 3 }
+      { x: 3, y: 2 }
     ];
     expect(JSON.stringify(result)).toBe(JSON.stringify(expects));
   });
--- packages/ts-node-editor/src/client/utils/mouse.ts
+++ packages/ts-node-editor/src/client/utils/mouse.ts
@@ -3,44 +3,136 @@ type Point = {
     y: number;
 }
 
+//heuristic we will be using - Manhattan distance
+//for other heuristics visit - https://theory.stanford.edu/~amitp/GameProgramming/Heuristics.html
+function heuristic(position0, position1) {
+    let d1 = Math.abs(position1.x - position0.x);
+    let d2 = Math.abs(position1.y - position0.y);
+    return d1 + d2;
+}
+
+class GridPoint {
+    x;
+    y;
+    cols;
+    rows;
+    walkable;
+    constructor(x, y, cols, rows, walkable) {
+        this.x = x; //x location of the grid point
+        this.y = y; //y location of the grid point
+        this.cols = cols;
+        this.rows = rows;
+        this.walkable = walkable;
+    }
+    f = 0; //total cost function
+    g = 0; //cost function from start to the current grid point
+    h = 0; //heuristic estimated cost function from current grid point to the goal
+    neighbors = []; // neighbors of the current grid point
+    parent = undefined; // immediate source of the current grid point
+    // update neighbors array for a given grid point
+    updateNeighbors(grid) {
+        let i = this.x;
+        let j = this.y;
+        if (i < this.cols - 1) {
+            this.neighbors.push(grid[i + 1][j]);
+        }
+        if (i > 0) {
+            this.neighbors.push(grid[i - 1][j]);
+        }
+        if (j < this.rows - 1) {
+            this.neighbors.push(grid[i][j + 1]);
+        }
+        if (j > 0) {
+            this.neighbors.push(grid[i][j - 1]);
+        }
+    }
+}
+
 /**
  * Navigate a 2d array of 0's and 1's.
  * 
  * @param matrix 2D array
- * @param start [x, y]
- * @param end [y, y]
- * @returns Array of x and y
+ * @param start { x, y }
+ * @param end { y, y }
+ * @returns Array of { x, y }
  */
 export function getPath(matrix: number[][], start: Point, end: Point): Point[] {
-    var queue = [];
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+    const grid = [];
+    let openSet = []; //array containing unevaluated grid points
+    let closedSet = []; //array containing completely evaluated grid points
 
-    matrix[start.x][start.y] = 1;
-    queue.push([[start.x, start.y]]);
+    let path = [];
 
-    while (queue.length > 0) {
-        var path = queue.shift();
-        var pos = path[path.length - 1];
-        var direction = [
-            [pos[0] + 1, pos[1]],
-            [pos[0], pos[1] + 1],
-            [pos[0] - 1, pos[1]],
-            [pos[0], pos[1] - 1]
-        ];
+    for (let i = 0; i < cols; i++) {
+        grid[i] = [];
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = new GridPoint(i, j, cols, rows, matrix[i][j] === 0);
+        }
+    }
 
-        for (var i = 0; i < direction.length; i++) {
-            if (direction[i][0] == end.x && direction[i][1] == end.y) {
-                path.push([end.x, end.y]);
-                return path.map(([y, x]) => ({ x, y }));
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j].updateNeighbors(grid);
+        }
+    }
+
+    const startGridPoint = grid[start.y][start.x];
+    const endGridPoint = grid[end.y][end.x];
+
+    openSet.push(startGridPoint);
+
+    while (openSet.length > 0) {
+        //assumption lowest index is the first one to begin with
+        let lowestIndex = 0;
+        for (let i = 0; i < openSet.length; i++) {
+            if (openSet[i].f < openSet[lowestIndex].f) {
+                lowestIndex = i;
             }
+        }
+        let current = openSet[lowestIndex];
 
-            if (direction[i][0] < 0 || direction[i][0] >= matrix.length
-                || direction[i][1] < 0 || direction[i][1] >= matrix[0].length
-                || matrix[direction[i][0]][direction[i][1]] != 0) {
+        if (current === endGridPoint) {
+            let temp = current;
+            path.push({ x: temp.y, y: temp.x });
+            while (temp.parent) {
+                path.push({ x: temp.parent.y, y: temp.parent.x });
+                temp = temp.parent;
+            }
+            console.log("DONE!");
+            // return the traced path
+            return path.reverse();
+        }
+
+        //remove current from openSet
+        openSet.splice(lowestIndex, 1);
+        //add current to closedSet
+        closedSet.push(current);
+
+        let neighbors = current.neighbors;
+
+        for (let i = 0; i < neighbors.length; i++) {
+            let neighbor = neighbors[i];
+            // skip any obstacles or neighbors that have already been evaluated
+            if (!neighbor.walkable || closedSet.includes(neighbor)) {
+                continue;
+            }
+            let possibleG = current.g + 1;
+
+            if (!openSet.includes(neighbor)) {
+                openSet.push(neighbor);
+            } else if (possibleG >= neighbor.g) {
                 continue;
             }
 
-            matrix[direction[i][0]][direction[i][1]] = 1;
-            queue.push(path.concat([direction[i]]));
+            neighbor.g = possibleG;
+            neighbor.h = heuristic(neighbor, endGridPoint);
+            neighbor.f = neighbor.g + neighbor.h;
+            neighbor.parent = current;
         }
     }
+
+    //no solution by default
+    return [];
 }
